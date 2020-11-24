a69dc93fdb7b85e80f91abf7939e4c1e1875335d
Vaibhav Kharatmal <67339364+lsdvaibhav@users.noreply.github.com>
2020-11-24 20:08:09 +0530

    Update qadash.py

--- qadash.py
+++ qadash.py
@@ -22,46 +22,47 @@ def login():
 
 def home():
   activities = ["EDA","Plots"]	
-  choice = st.sidebar.selectbox("Select Activities",activities)
+  choice = st.selectbox("Select Activities",activities)
+  if choice == 'EDA':
+		st.subheader("Exploratory Data Analysis")
+    data = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
+    if data is not None:
+      df = pd.read_csv(data)
+      st.dataframe(df.head())
 
-  data = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
-  if data is not None:
-    df = pd.read_csv(data)
-    st.dataframe(df.head())
+      if st.checkbox("Show Shape"):
+        st.write(df.shape)
 
-    if st.checkbox("Show Shape"):
-      st.write(df.shape)
+      if st.checkbox("Show Columns"):
+        all_columns = df.columns.to_list()
+        st.write(all_columns)
 
-    if st.checkbox("Show Columns"):
-      all_columns = df.columns.to_list()
-      st.write(all_columns)
+      if st.checkbox("Summary"):
+        st.write(df.describe())
 
-    if st.checkbox("Summary"):
-      st.write(df.describe())
+      if st.checkbox("Show Selected Columns"):
+        selected_columns = st.multiselect("Select Columns",all_columns)
+        new_df = df[selected_columns]
+        st.dataframe(new_df)
 
-    if st.checkbox("Show Selected Columns"):
-      selected_columns = st.multiselect("Select Columns",all_columns)
-      new_df = df[selected_columns]
-      st.dataframe(new_df)
+      if st.checkbox("Show Value Counts"):
+        st.write(df.iloc[:,-1].value_counts())
 
-    if st.checkbox("Show Value Counts"):
-      st.write(df.iloc[:,-1].value_counts())
+      if st.checkbox("Correlation Plot(Matplotlib)"):
+        plt.matshow(df.corr())
+        st.pyplot()
 
-    if st.checkbox("Correlation Plot(Matplotlib)"):
-      plt.matshow(df.corr())
-      st.pyplot()
-
-    if st.checkbox("Correlation Plot(Seaborn)"):
-      st.write(sns.heatmap(df.corr(),annot=True))
-      st.pyplot()
+      if st.checkbox("Correlation Plot(Seaborn)"):
+        st.write(sns.heatmap(df.corr(),annot=True))
+        st.pyplot()
 
 
-    if st.checkbox("Pie Plot"):
-      all_columns = df.columns.to_list()
-      column_to_plot = st.selectbox("Select 1 Column",all_columns)
-      pie_plot = df[column_to_plot].value_counts().plot.pie(autopct="%1.1f%%")
-      st.write(pie_plot)
-      st.pyplot()
+      if st.checkbox("Pie Plot"):
+        all_columns = df.columns.to_list()
+        column_to_plot = st.selectbox("Select 1 Column",all_columns)
+        pie_plot = df[column_to_plot].value_counts().plot.pie(autopct="%1.1f%%")
+        st.write(pie_plot)
+        st.pyplot()
 
 
 
