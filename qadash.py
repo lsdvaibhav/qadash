8f5a8782381c222b512189e5c077b6c4ddaac049
Vaibhav Kharatmal <67339364+lsdvaibhav@users.noreply.github.com>
2020-11-24 19:31:20 +0530

    Update qadash.py

--- qadash.py
+++ qadash.py
@@ -62,45 +62,45 @@ def home():
 
 
 
-elif choice == 'Plots':
-  st.subheader("Data Visualization")
-  data = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
-  if data is not None:
-    df = pd.read_csv(data)
-    st.dataframe(df.head())
+  elif choice == 'Plots':
+    st.subheader("Data Visualization")
+    data = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
+    if data is not None:
+      df = pd.read_csv(data)
+      st.dataframe(df.head())
 
 
-    if st.checkbox("Show Value Counts"):
-      st.write(df.iloc[:,-1].value_counts().plot(kind='bar'))
-      st.pyplot()
-
-    # Customizable Plot
-
-    all_columns_names = df.columns.tolist()
-    type_of_plot = st.selectbox("Select Type of Plot",["area","bar","line","hist","box","kde"])
-    selected_columns_names = st.multiselect("Select Columns To Plot",all_columns_names)
-
-    if st.button("Generate Plot"):
-      st.success("Generating Customizable Plot of {} for {}".format(type_of_plot,selected_columns_names))
-
-      # Plot By Streamlit
-      if type_of_plot == 'area':
-        cust_data = df[selected_columns_names]
-        st.area_chart(cust_data)
-
-      elif type_of_plot == 'bar':
-        cust_data = df[selected_columns_names]
-        st.bar_chart(cust_data)
-
-      elif type_of_plot == 'line':
-        cust_data = df[selected_columns_names]
-        st.line_chart(cust_data)
-
-      # Custom Plot 
-      elif type_of_plot:
-        cust_plot= df[selected_columns_names].plot(kind=type_of_plot)
-        st.write(cust_plot)
+      if st.checkbox("Show Value Counts"):
+        st.write(df.iloc[:,-1].value_counts().plot(kind='bar'))
         st.pyplot()
+
+      # Customizable Plot
+
+      all_columns_names = df.columns.tolist()
+      type_of_plot = st.selectbox("Select Type of Plot",["area","bar","line","hist","box","kde"])
+      selected_columns_names = st.multiselect("Select Columns To Plot",all_columns_names)
+
+      if st.button("Generate Plot"):
+        st.success("Generating Customizable Plot of {} for {}".format(type_of_plot,selected_columns_names))
+
+        # Plot By Streamlit
+        if type_of_plot == 'area':
+          cust_data = df[selected_columns_names]
+          st.area_chart(cust_data)
+
+        elif type_of_plot == 'bar':
+          cust_data = df[selected_columns_names]
+          st.bar_chart(cust_data)
+
+        elif type_of_plot == 'line':
+          cust_data = df[selected_columns_names]
+          st.line_chart(cust_data)
+
+        # Custom Plot 
+        elif type_of_plot:
+          cust_plot= df[selected_columns_names].plot(kind=type_of_plot)
+          st.write(cust_plot)
+          st.pyplot()
 
 
 if __name__ == "__main__":
