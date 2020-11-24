5d6853d8b39c52c6680899e2f2786719160b2ac7
Vaibhav Kharatmal <67339364+lsdvaibhav@users.noreply.github.com>
2020-11-24 18:14:45 +0530

    Create qadash.py

---
+++ qadash.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def login():
+  """login form""""
+  st.title("qaDash")
+  st.subheader("Login")
+  username = st.text_input("Username")
+  password = st.text_input("Password", type='password')
+  if st.button("Login"):
+    if  password == '123': 
+      st.success("logged in")
+    
+if __name__ = __main__:
+  login()
