b43af47ecdfaa5959995fd226d79082ccf740857
Vaibhav Kharatmal <67339364+lsdvaibhav@users.noreply.github.com>
2020-11-27 13:40:02 +0530

    Update qadash.py

--- qadash.py
+++ qadash.py
@@ -24,21 +24,22 @@ def main():
 	y1 = [20,40,10]
 	# plotting the line 1 points 
 	plt.style.use('seaborn-darkgrid')
-	plt.plot(x1, y1, marker='o', markersize=10, linewidth=2, label = "line 1")
+	fig ,ax  = plt.subplots()
+	ax.plot(x1, y1, marker='o', markersize=10, linewidth=2, label = "line 1")
 	# line 2 points
 	x2 = [10,20,30]
 	y2 = [40,10,30]
 	# plotting the line 2 points 
-	plt.plot(x2, y2, marker='o', markersize=10, linewidth=2, label = "line 2")
-	plt.xlabel('x - axis')
+	ax.plot(x2, y2, marker='o', markersize=10, linewidth=2, label = "line 2")
+	ax.xlabel('x - axis')
 	# Set the y axis label of the current axis.
-	plt.ylabel('y - axis')
+	ax.ylabel('y - axis')
 	# Set a title of the current axes.
-	plt.title('Two or more lines on same plot with suitable legends ')
+	ax.title('Two or more lines on same plot with suitable legends ')
 	# show a legend on the plot
-	plt.legend()
+	ax.legend()
 	# Display a figure.
-	fig ,ax  = plt.subplots()
+	
 	st.pyplot(fig)
 	
 	if choice == 'EDA':
